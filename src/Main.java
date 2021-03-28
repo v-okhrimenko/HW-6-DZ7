71ed7bc46dc0671dc35859af4e6f98f667fef6d8
vasyl_okhrimenko <v.okhrimenko@gmail.com>
2021-03-28 15:09:38 +0300

    -

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,43 @@
-import java.util.Arrays;
-import java.util.concurrent.Phaser;
-
 public class Main {
     private static final CustomersArray customersArray = new CustomersArray();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
 
-        Phone ph1 = new Phone("Ivanov", "Vasiliy", "Petrovych",
-                new Address("u", "kiev", "Avenue", "13f", 123),
+        Phone ph1 = new Phone("Бойко", "Vasiliy", "Petrovych",
+                new Phone.Address("u", "kiev", "Avenue", "13f", 123),
                 "380634569707", 1212121, 34212, 121, 77,
                 0, 12314);
 
-        Phone ph2 = new Phone("Petrov", "Alex", "Nikolayevych",
-                new Address("Ukraine", "Lviv", "Khmelnickogo", "1a", 3),
+        Phone ph2 = new Phone("Асад", "Alex", "Nikolayevych",
+                new Phone.Address("Ukraine", "Lviv", "Khmelnickogo", "1a", 3),
                 "380934567832", 1212121, 121212, 121, 10,
                 1, 1231);
 
-        Phone ph3 = new Phone("______", "Alex", "Nikolayevych",
-                new Address("Ukraine", "Lviv", "Khmelnickogo", "1a", 3),
+        Phone ph3 = new Phone("Дмитрук", "Alex", "Nikolayevych",
+                new Phone.Address("Ukraine", "Lviv", "Khmelnickogo", "1a", 3),
                 "380934567832", 1212121, 121212, 121, 1,
                 0, 1231);
+
+
+        Phone ph = new Phone("Вилкул", "Василий", "Александрович",
+                new Phone.Address("Uk", "Kiev", "Labooda", "10a", 122), "1212121212",
+                12345, 1222, 32323, 122, 12, 22);
 
 
         customersArray.saveCustomer(ph1);
         customersArray.saveCustomer(ph2);
         customersArray.saveCustomer(ph3);
+        customersArray.saveCustomer(ph);
+        Phone p = new Phone();
+        customersArray.saveCustomer(p);
 
+        //System.out.println(customersArray.toString());
+        System.out.println("______________ _______________ ____________");
+        PhoneMetods pm = new CustomersArray();
+        //pm.getCustomersUsedNoLocalCallMoreThan(1);
 
-        System.out.println(ph3);
-
-
-        //PhoneMetods pm = new CustomersArray();
-        //pm.getCustomerUseNoLocalCall();
-        //pm.getCustomerMoreLocalCall(2);
-        //pm.sortCustomerByName();
+        //pm.getCustomersUsedNoLocalCall();
+        pm.sortCustomerByName();
 
     }
 }
